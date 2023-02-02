5df5bafb0109f91c093e3a23e9f95101486d3903
Hannes Gräuler <hannes@smasi.de>
2023-02-02 16:52:45 +0100

    Format and add more CityGML types

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code)]
 
 extern crate pretty_env_logger;
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 
 use std::collections::HashMap;
 use std::fs::File;
@@ -9,12 +10,12 @@ use std::io::Write;
 use std::path::Path;
 // use cjval;
 
+use indexmap::IndexSet;
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
-use indexmap::IndexSet;
 use rust_decimal::Decimal;
-use serde::{Serialize, Deserialize};
 use serde::ser::SerializeMap;
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Eq, Hash, PartialEq, Clone)]
 struct Vertex([Decimal; 3]);
@@ -45,33 +46,66 @@ impl TriangleIndices {
 
 #[derive(Serialize, Deserialize)]
 enum CityObjectType {
+    // All CityGML 2.0 types
+    CityFurniture,
+    CityObjectGroup,
+    GenericCityObject,
+    LandUse,
+    PlantCover,
+    SolitaryVegetationObject,
+    WaterBody,
+    AuxiliaryTrafficArea,
+    Railway,
+    Road,
+    Track,
+    TrafficArea,
+    Tunnel,
+    Bridge,
+    BridgeConstructionElement,
+    BridgeFurniture,
+    BridgeInstallation,
+    BridgePart,
+    BridgeRoom,
+    BridgeRoofSurface,
+    BridgeWallSurface,
     Building,
     BuildingInstallation,
-    Railway,
-    TINRelief
+    TINRelief,
 }
 
 #[derive(Serialize, Deserialize)]
 enum CityObjectGeometryType {
+    // All CityGML 2.0 geometry types
+    CompositeSolid,
+    MultiSolid,
+    MultiCurve,
+    MultiPoint,
+    MultiSolidCoverage,
+    MultiSurfaceCoverage,
+    MultiCurveCoverage,
+    MultiPointCoverage,
+    CompositeCurve,
+    CompositePoint,
     CompositeSurface,
-    MultiSurface
+    MultiSurface,
 }
 
 #[derive(Clone, Copy)]
 enum CityObjectGeometryLOD {
     One,
     Two,
-    Three
+    Three,
 }
 
 impl Serialize for CityObjectGeometryLOD {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
-        S: serde::Serializer {
+        S: serde::Serializer,
+    {
         match self {
             CityObjectGeometryLOD::One => serializer.serialize_str("1"),
             CityObjectGeometryLOD::Two => serializer.serialize_str("2"),
-            CityObjectGeometryLOD::Three => serializer.serialize_str("3")
+            CityObjectGeometryLOD::Three => serializer.serialize_str("3"),
         }
     }
 }
@@ -79,13 +113,14 @@ impl Serialize for CityObjectGeometryLOD {
 impl<'de> Deserialize<'de> for CityObjectGeometryLOD {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
-        D: serde::Deserializer<'de> {
+        D: serde::Deserializer<'de>,
+    {
         let s = String::deserialize(deserializer)?;
         match s.as_str() {
             "1" => Ok(CityObjectGeometryLOD::One),
             "2" => Ok(CityObjectGeometryLOD::Two),
             "3" => Ok(CityObjectGeometryLOD::Three),
-            _ => Err(serde::de::Error::custom("Invalid CityObjectGeometryLOD"))
+            _ => Err(serde::de::Error::custom("Invalid CityObjectGeometryLOD")),
         }
     }
 }
@@ -97,20 +132,20 @@ struct Boundary(Vec<TriangleIndices>);
 struct CityObjectGeometry {
     r#type: CityObjectGeometryType,
     lod: CityObjectGeometryLOD,
-    boundaries: Vec<Boundary>
+    boundaries: Vec<Boundary>,
 }
 
 #[derive(Serialize, Deserialize)]
 struct CityObject {
     r#type: CityObjectType,
-    geometry: Vec<CityObjectGeometry>
+    geometry: Vec<CityObjectGeometry>,
 }
 
 impl CityObject {
     pub(crate) fn new(r#type: CityObjectType) -> CityObject {
         CityObject {
             r#type,
-            geometry: vec![]
+            geometry: vec![],
         }
     }
 }
@@ -126,13 +161,13 @@ struct CityModel {
     #[serde(rename = "CityObjects")]
     objects: HashMap<String, CityObject>,
     transform: CityModelTransform,
-    vertices: IndexSet<Vertex>
+    vertices: IndexSet<Vertex>,
 }
 
 impl Serialize for CityModel {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: serde::Serializer,
+    where
+        S: serde::Serializer,
     {
         let mut obj = serializer.serialize_map(None)?;
         obj.serialize_entry("type", "CityJSON")?;
@@ -145,8 +180,7 @@ impl Serialize for CityModel {
 }
 
 fn parse(s: &str) -> Vec<Vertex> {
-    s
-        .split_whitespace()
+    s.split_whitespace()
         .map(|num| num.parse().unwrap())
         .collect::<Vec<Decimal>>()
         .chunks(3)
@@ -173,41 +207,49 @@ impl CityModel {
                         b"gml:posList" => collect = true,
                         b"dem:TINRelief" => {
                             current_cityobject = Some(CityObject::new(CityObjectType::TINRelief))
-                        },
+                        }
                         _ => (),
                     }
                 }
 
-                Ok(Event::End(e)) => {
-                    match e.name().as_ref() {
-                        b"gml:posList" => collect = false,
-                        b"dem:TINRelief" => {
-                            if let Some(mut co) = current_cityobject {
-                                co.geometry = vec![CityObjectGeometry {
-                                    r#type: CityObjectGeometryType::CompositeSurface,
-                                    lod: CityObjectGeometryLOD::Two,
-                                    boundaries: triangles.into_iter().map(|t| Boundary(vec![t])).collect()
-                                }];
-                                model.objects.insert("ground".to_string(), co);
-                            }
-                            current_cityobject = None;
-                            triangles = Vec::new()
-                        },
-                _ => (),
+                Ok(Event::End(e)) => match e.name().as_ref() {
+                    b"gml:posList" => collect = false,
+                    b"dem:TINRelief" => {
+                        if let Some(mut co) = current_cityobject {
+                            co.geometry = vec![CityObjectGeometry {
+                                r#type: CityObjectGeometryType::CompositeSurface,
+                                lod: CityObjectGeometryLOD::Two,
+                                boundaries: triangles
+                                    .into_iter()
+                                    .map(|t| Boundary(vec![t]))
+                                    .collect(),
+                            }];
+                            model.objects.insert("ground".to_string(), co);
+                        }
+                        current_cityobject = None;
+                        triangles = Vec::new()
                     }
-                }
+                    _ => (),
+                },
 
                 Ok(Event::Text(e)) => {
                     if collect {
                         // dbg!(parse(&e.unescape().unwrap().into_owned()));
                         let my_vertices = parse(&e.unescape().unwrap().into_owned());
                         let triangle = TriangleIndices::from_vec(
-                            my_vertices.into_iter().take(3).map(|vertex| { let (index, _) = model.vertices.insert_full(vertex); index }).collect()
+                            my_vertices
+                                .into_iter()
+                                .take(3)
+                                .map(|vertex| {
+                                    let (index, _) = model.vertices.insert_full(vertex);
+                                    index
+                                })
+                                .collect(),
                         );
                         triangles.push(triangle);
                     }
                     ()
-                },
+                }
 
                 Ok(Event::Eof) => break,
 
@@ -225,15 +267,17 @@ impl CityModel {
             objects: HashMap::new(),
             transform: CityModelTransform {
                 scale: [Decimal::ONE, Decimal::ONE, Decimal::ONE],
-                translate: [Decimal::ZERO, Decimal::ZERO, Decimal::ZERO]
+                translate: [Decimal::ZERO, Decimal::ZERO, Decimal::ZERO],
             },
-            vertices: IndexSet::new()
+            vertices: IndexSet::new(),
         }
     }
 }
 
 fn main() {
-    pretty_env_logger::formatted_timed_builder().filter_level(log::LevelFilter::Info).init();
+    pretty_env_logger::formatted_timed_builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
 
     // Read input from the program arguments
     let input = std::env::args().nth(1).expect("Input file");
@@ -243,7 +287,11 @@ fn main() {
     let input_size = std::fs::metadata(&input).unwrap().len() as f64 / 1024.0 / 1024.0;
     info!("Loading CityGML: {} MB", input_size);
     let model = CityModel::from_file(input.as_ref()).expect("Valid CityGML");
-    info!("Loaded CityModel with {} objects and {} vertices", model.objects.len(), model.vertices.len());
+    info!(
+        "Loaded CityModel with {} objects and {} vertices",
+        model.objects.len(),
+        model.vertices.len()
+    );
     info!("Deserializing to CityJSON");
     let s = serde_json::to_string(&model).unwrap();
     info!("Size of CityJSON: {} MB", s.len() as f64 / 1024.0 / 1024.0);
@@ -255,9 +303,13 @@ fn main() {
     info!("Loading CityJSON again");
     let s2 = std::fs::read_to_string(&output_path).unwrap();
     let model2: CityModel = serde_json::from_str(&s2).unwrap();
-    info!("Loaded CityModel with {} objects and {} vertices", model2.objects.len(), model2.vertices.len());
+    info!(
+        "Loaded CityModel with {} objects and {} vertices",
+        model2.objects.len(),
+        model2.vertices.len()
+    );
 
-               // let s1 = std::fs::read_to_string("data.json")
+    // let s1 = std::fs::read_to_string("data.json")
     //         .expect("Couldn't read CityJSON file");
     // let v = cjval::CJValidator::from_str(&s1);
     // if v.is_valid() {
